bba70d0a970bb6aaf6b9702fb825b298163a21ea
root <digimarketindo@gmail.com>
2026-05-22 01:32:12 +0000

    Prepare frontend for Vercel deployment

--- backend/src/modules/admin/admin.service.ts
+++ backend/src/modules/admin/admin.service.ts
@@ -193,7 +193,6 @@ export class AdminService {
               fullName: true,
             },
           },
-          plan: true,
           bankAccount: true,
         },
         orderBy: { createdAt: 'desc' },
